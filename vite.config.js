0825fe8a15e12c79a574ebe179d93a6105546a64
yeji424 <yejiborn424@gmail.com>
2025-04-29 14:40:14 +0900

    feat: 배포 환경 설정

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
+  base: '/practice-api/',
   plugins: [react()],
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx'],
